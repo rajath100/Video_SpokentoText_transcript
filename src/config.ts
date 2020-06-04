487f5bd75e9c9853e769e6ab9917464d6863e292
RAJATH RAVINDRANATH SHIGGAONKAR <57888667+rajath100@users.noreply.github.com>
2020-06-04 18:51:26 +0530

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+export let config = {
+    speech: {
+        bingSpeech: {
+            endPoind: "https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/V1?language=en-us&format=detailed",
+            authEndPoint: "https://api.cognitive.microsoft.com/sts/v1.0",
+            key1: "",
+            key2: "",
+            synthesizeUrl: "https://speech.platform.bing.com/synthesize"
+        },
+        translateApi: {
+            endPoind: "https://api.microsofttranslator.com/V2/Http.Svc",
+            authEndPoint: "https://api.cognitive.microsoft.com/sts/v1.0",
+            key1: "",
+            key2: ""
+        }
+    }
+};
